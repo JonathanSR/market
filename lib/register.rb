a4a6e2c357e1f5f22f6b5d4911c031b74ee9f4ef
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-09-13 13:00:10 -0600

    creates methods for each promo code to be applied to the total checkout price

--- lib/register.rb
+++ lib/register.rb
@@ -1,8 +1,10 @@
 class Register
-  attr_reader :basket
+  attr_reader :basket,
+              :total
 
   def initialize
     @basket = Basket.new
+    @total = 0 
   end
 
   def checks_for_apple_discounts
@@ -15,19 +17,63 @@ class Register
   def checks_for_chai_discounts
     if basket.sheet.key?("CH1") && basket.sheet["CH1"][:quantity] == 1
       basket.add_item_to_inventory(Milk.new)
-      basket.sheet["MK1"].merge!(CHMK: -4.50)
+      basket.sheet["MK1"].merge!(CHMK: -4.75)
     else
     end
   end
 
-  def checks_for_coffee_discount
+  def checks_for_coffee_discounts
     if basket.sheet.key?("CF1")
+      basket.add_item_to_inventory(Coffee.new)
       basket.sheet["CF1"].merge!(COFF: -11.23)
     else
     end
   end
 
-    #checks if basket has coffee
-      #adds a coffee for every coffee originally added
 
+  def checks_for_discounts
+    checks_for_apple_discounts
+    checks_for_chai_discounts
+    checks_for_coffee_discounts
+  end
+
+  def get_total
+    first = basket.sheet.values.map {|s| [s[:price], s[:quantity]]}
+    second = first.map{|s| s.inject(:*)}
+    @total = second.inject(0){|sum, x| sum + x}
+  end
+
+  def apply_coffee_discount
+    if basket.sheet.key?("CF1") && basket.sheet["CF1"].key?(:COFF)
+      first = (basket.sheet["CF1"][:price] * basket.sheet["CF1"][:quantity]) + ((basket.sheet["CF1"][:quantity] /2 ) * basket.sheet["CF1"][:COFF])
+      @total = first
+    else
+      @total = basket.sheet["CF1"][:price] * register.basket.sheet["CF1"][:quantity]
+    end
+  end
+
+
+  def apply_chai_discount
+    if basket.sheet.key?("CH1") && basket.sheet["MK1"].key?(:CHMK)
+      chai = basket.sheet["CH1"][:price] * basket.sheet["CH1"][:quantity]
+      milk = (basket.sheet["MK1"][:price] * basket.sheet["MK1"][:quantity] + basket.sheet["MK1"][:CHMK])
+      @total = chai + milk
+    else
+      @total = basket.sheet["CH1"][:price] * register.basket.sheet["CH1"][:quantity]    
+    end
+  end
+
+  def apply_apples_discount
+    if basket.sheet.key?("AP1") && basket.sheet["AP1"].key?(:APPL)
+      original_price = basket.sheet["AP1"][:price] * basket.sheet["AP1"][:quantity]
+      discount_amount  = basket.sheet["AP1"][:APPL] * basket.sheet["AP1"][:quantity]
+      @total = original_price + discount_amount
+    else
+      @total = basket.sheet["AP1"][:price] * basket.sheet["AP1"][:quantity]
+    end
+  end  
 end
+
+
+#adds up total for basket
+#adds up total for basket including promos
--- test/register_test.rb
+++ test/register_test.rb
@@ -48,7 +48,7 @@ class RegisterTest < Minitest::Test
     register.basket.add_item_to_inventory(chai)
     register.checks_for_chai_discounts
 
-    assert_equal ({"CH1"=>{:name=>"Chai", :price=>3.11, :quantity=>1}, "MK1"=>{:name=>"Milk", :price=>4.75, :quantity=>1, :CHMK=>-4.5}}), register.basket.sheet
+    assert_equal ({"CH1"=>{:name=>"Chai", :price=>3.11, :quantity=>1}, "MK1"=>{:name=>"Milk", :price=>4.75, :quantity=>1, :CHMK=>-4.75}}), register.basket.sheet
   end
 
   def test_it_applies_chai_milk_discount_only_once
@@ -58,21 +58,128 @@ class RegisterTest < Minitest::Test
     register.basket.add_item_to_inventory(chai)
     register.checks_for_chai_discounts
 
-    assert_equal ({"CH1"=>{:name=>"Chai", :price=>3.11, :quantity=>1}, "MK1"=>{:name=>"Milk", :price=>4.75, :quantity=>1, :CHMK=>-4.5}}), register.basket.sheet
+    assert_equal ({"CH1"=>{:name=>"Chai", :price=>3.11, :quantity=>1}, "MK1"=>{:name=>"Milk", :price=>4.75, :quantity=>1, :CHMK=>-4.75}}), register.basket.sheet
 
     register.basket.add_item_to_inventory(chai)
 
-    assert_equal ({"CH1"=>{:name=>"Chai", :price=>3.11, :quantity=>2}, "MK1"=>{:name=>"Milk", :price=>4.75, :quantity=>1, :CHMK=>-4.5}}), register.basket.sheet
+    assert_equal ({"CH1"=>{:name=>"Chai", :price=>3.11, :quantity=>2}, "MK1"=>{:name=>"Milk", :price=>4.75, :quantity=>1, :CHMK=>-4.75}}), register.basket.sheet
   end
 
-  def test_it_checks_and_applies_coffee_discount
+  def test_it_checks_and_applies_coffee_discounts
     register = Register.new
     coffee = Coffee.new
 
     register.basket.add_item_to_inventory(coffee)
 
-    register.checks_for_coffee_discount
+    register.checks_for_coffee_discounts
 
-    assert_equal ({"CF1"=>{:name=>"Coffee", :price=>11.23, :quantity=>1, :COFF=>-11.23}}), register.basket.sheet
+    assert_equal ({"CF1"=>{:name=>"Coffee", :price=>11.23, :quantity=>2, :COFF=>-11.23}}), register.basket.sheet
   end
+
+  def test_it_checks_for_all_discounts
+    register = Register.new
+    coffee = Coffee.new
+    apple = Apples.new
+    milk = Milk.new
+    chai = Chai.new
+
+    register.basket.add_item_to_inventory(apple)
+    register.checks_for_discounts
+    assert_equal ({"AP1"=>{:name=>"Apples", :price=>6.00, :quantity=>1}}), register.basket.sheet
+    
+    register.basket.add_item_to_inventory(apple)
+    register.basket.add_item_to_inventory(apple)
+    register.checks_for_discounts 
+    assert_equal ({"AP1"=>{:name=>"Apples", :price=>6.00, :quantity=>3, :APPL=>-1.5}}), register.basket.sheet
+    
+    register.basket.add_item_to_inventory(coffee)
+    register.basket.add_item_to_inventory(milk)
+    register.basket.add_item_to_inventory(chai)
+    register.checks_for_discounts
+    assert_equal ({"AP1"=>{:name=>"Apples", :price=>6.0, :quantity=>3, :APPL=>-1.5}, "CF1"=>{:name=>"Coffee", :price=>11.23, :quantity=>2, :COFF=>-11.23}, "MK1"=>{:name=>"Milk", :price=>4.75, :quantity=>2, :CHMK=>-4.75}, "CH1"=>{:name=>"Chai", :price=>3.11, :quantity=>1}}), register.basket.sheet
+    
+  end
+
+
+  def test_it_begins_with_a_0_total
+    register = Register.new
+
+    assert_equal 0, register.total
+  end
+
+  def test_it_adds_up_total_of_basket_case_1
+    register = Register.new
+    apple = Apples.new
+    chai = Chai.new
+
+    register.basket.add_item_to_inventory(apple)
+    register.basket.add_item_to_inventory(chai)
+    # binding.pry
+    assert_equal 9.11, register.get_total
+  end
+
+  def test_it_applies_coffee_discount_to_total
+    register = Register.new
+    coffee = Coffee.new
+
+    register.basket.add_item_to_inventory(coffee)
+    register.checks_for_discounts
+
+    assert_equal 11.23, register.apply_coffee_discount
+    assert_equal ({"CF1"=>{:name=>"Coffee", :price=>11.23, :quantity=>2, :COFF=>-11.23}}), register.basket.sheet
+
+    register.basket.add_item_to_inventory(coffee)
+    register.checks_for_discounts
+
+    assert_equal 22.46, register.apply_coffee_discount
+    assert_equal ({"CF1"=>{:name=>"Coffee", :price=>11.23, :quantity=>4, :COFF=>-11.23}}), register.basket.sheet
+  end
+
+  def test_it_applies_chai_milk_discount_to_total
+    register = Register.new
+    chai = Chai.new
+
+    register.basket.add_item_to_inventory(chai)
+    register.checks_for_discounts
+# binding.pry
+    assert_equal 3.11, register.apply_chai_discount
+
+    register.basket.add_item_to_inventory(chai)
+    register.checks_for_discounts
+
+    assert_equal 6.22, register.apply_chai_discount
+  end
+
+  def test_it_applies_apple_discount_to_total
+    register = Register.new
+    apple = Apples.new
+
+    register.basket.add_item_to_inventory(apple)
+    register.basket.add_item_to_inventory(apple)
+    register.checks_for_discounts
+    assert_equal 12.00, register.apply_apples_discount 
+    
+    register.basket.add_item_to_inventory(apple)
+    register.checks_for_discounts
+# binding.pry
+    assert_equal 13.50, register.apply_apples_discount 
+  end
+  # def test_it_adds_up_total_of_basket_case_2
+  #   register = Register.new
+  #   apple = Apples.new
+  #   chai = Chai.new
+  #   milk = Milk.new
+  #   coffee = Coffee.new
+
+  #   register.basket.add_item_to_inventory(apple)
+  #   register.basket.add_item_to_inventory(chai)
+  #   register.basket.add_item_to_inventory(milk)
+  #   register.basket.add_item_to_inventory(coffee)
+  #   register.checks_for_discounts
+
+  #   binding.pry
+  #   assert_equal 20.59, register.get_total
+  # end
+
+
 end
